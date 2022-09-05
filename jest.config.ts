7b8a4404340650a40172fae01d41762445f341a5
AliZhakezhanov1 <ali@midasanalytics.ai>
2022-09-05 10:55:08 +0600

    feat: setTemplate

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+process.env.NODE_ENV = "UNITTEST";
+
+module.exports = {
+	clearMocks: true,
+	collectCoverage: true,
+	collectCoverageFrom: ["./src/**/*.ts"],
+	coverageDirectory: "<rootDir>/test/coverage",
+	testEnvironment: "node",
+	testMatch: ["**/*.test.ts"],
+	preset: "ts-jest",
+	setupFilesAfterEnv: ["./jest.setup.ts"],
+};
+
